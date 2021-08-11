011dc9d07d50adc132465e97ef578103f90673c7
Iury Vasconcelos <iury0393@gmail.com>
2021-08-11 11:29:49 -0300

    test: commit

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -2,7 +2,7 @@ import { HttpPostClient, HttpPostParams } from 'data/protocols/http/http-post-cl
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
-
+//Test
   async post (params: HttpPostParams): Promise<void> {
     this.url = params.url
     return await Promise.resolve()
